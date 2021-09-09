eb47ab6fc577fea07d2704847d189b1323c10e6e
dedeklender <43282734+dedeklender@users.noreply.github.com>
2021-09-09 17:56:12 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+# mengimport package pyTelegramBotAPI
+import telebot
+
+# inisialisasi Token Bot Kita
+bot = telebot.TeleBot('1516072532:AAF9hsylO3bknwo_llauZhAVLhNCXvoiIHQ')
+
+# Menghandle Pesan /start
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    # membalas pesan
+    bot.reply_to(message, 'Halo bro, ada apa?')
+
+while True:
+    try:
+        bot.polling()
+    except:
+        pass
